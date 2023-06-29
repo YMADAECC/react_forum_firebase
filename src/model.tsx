e275e99c625aa5b0a52784b1db5f4dbe304567f0
geh657 <2200515@ecc.ac.jp>
2023-06-29 12:09:01 +0900

    feat : データモデルの内容を変更

---
+++ src/model.tsx
@@ -0,0 +1,14 @@
+export type IUser = {
+    displayName: string | null | undefined
+    photoURL: string | null | undefined
+}
+export type IComment = {
+    user: IUser
+    content: string
+    createdAt: Date
+    id: string
+}
+export type ICommentAdd = {
+    user: IUser
+    content: string
+}
